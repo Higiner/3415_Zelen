48b9010252c27fdf64c25c3d66cd4bfad4cd249b
Higiner <aleksandraleskin68@gmail.com>
2024-09-16 19:50:46 +0300

    Card: изменил отображение карт, score + test

--- src/card.py
+++ src/card.py
@@ -2,58 +2,30 @@
 
 class Card:
 
-    def __init__(self, corn = 0, eggplant = 0, broccoli = 0, tomato = 0, carrot = 0):
-        if corn + eggplant + broccoli + tomato + carrot != 3:
+    def __init__(self, к = 0, о = 0, б = 0, т = 0, м = 0):
+        if к + о + б + т + м != 3:
             raise ValueError
-        self.Corn = corn
-        self.Eggplant = eggplant
-        self.Broccoli = broccoli
-        self.Tomato = tomato
-        self.Carrot = carrot
+        self.к = к
+        self.о = о
+        self.б = б
+        self.т = т
+        self.м = м
 
     def __repr__(self):
-        st = ''
-        if self.Corn != 0:
-            st += f'Corn - {self.Corn}; '
-        if self.Eggplant != 0:
-            st += f'Eggplant - {self.Eggplant}; '
-        if self.Broccoli != 0:
-            st += f'Broccoli - {self.Broccoli}; '
-        if self.Tomato != 0:
-            st += f'Tomato - {self.Tomato}; '
-        if self.Carrot != 0:
-            st += f'Carrot - {self.Carrot}; '
-        return st[:-2]
+        return "к" * self.к + "о" * self.о + "б" * self.б + "т" * self.т + "м" * self.м
 
     def __eq__(self, other):
-        return self.Carrot == other.Carrot and self.Corn == other.Corn \
-            and self.Tomato == other.Tomato and self.Broccoli == other.Broccoli and self.Eggplant == other.Eggplant
+        return self.к == other.к and self.м == other.м \
+            and self.т == other.т and self.б == other.б and self.о == other.о
+
+    def score(self, other):
+        return self.к * other.к + self.о * other.о + self.б * other.б + self.т * other.т + self.м * other.м
 
     def save(self):
         return repr(self)
 
     @staticmethod
     def load(text: str):
-        """From 'Tomato - 3' to Card(tomato=3)"""
-        if text.find("Corn - ") != -1:
-            corn = int(text[text.find("Corn - ") + 6: text.find("Corn - ") + 8])
-        else:
-            corn = 0
-        if text.find("Eggplant - ") != -1:
-            eggplant = int(text[text.find("Eggplant - ") + 10: text.find("Eggplant - ") + 12])
-        else:
-            eggplant = 0
-        if text.find("Brocoli - ") != -1:
-            brocoli = int(text[text.find("Brocoli - ") + 9: text.find("Brocoli - ") + 11])
-        else:
-            brocoli = 0
-        if text.find("Tomato - ") != -1:
-            tomato = int(text[text.find("Tomato - ") + 8: text.find("Tomato - ") + 10])
-        else:
-            tomato = 0
-        if text.find("Carrot - ") != -1:
-            carrot = int(text[text.find("Carrot - ") + 8: text.find("Carrot - ") + 10])
-        else:
-            carrot = 0
-        return Card(corn, eggplant, brocoli, tomato, carrot)
+        """From 'ттт' to Card(т=3)"""
+        return Card(text.count("к"), text.count("о"), text.count("б"), text.count("т"), text.count("м"))
 
--- test/test_card.py
+++ test/test_card.py
@@ -1,23 +1,30 @@
 import pytest
 
 from src.card import Card
+from src.price import Price
+
 
 def test_init():
-    c = Card(corn=2, carrot=1)
-    assert c.Corn == 2
-    assert c.Carrot == 1
+    c = Card(к=2, м=1)
+    assert c.к == 2
+    assert c.м == 1
 
 def test_validation():
     with pytest.raises(ValueError):
-        Card(tomato=4)
-        Card(tomato=1, eggplant=1, carrot=1, corn=1)
+        Card(т=4)
+        Card(т=1, о=1, м=1, к=1)
 
 def test_save():
-    c = Card(tomato=1, eggplant=2)
-    assert repr(c) == "Eggplant - 2; Tomato - 1"
-    assert c.save() == "Eggplant - 2; Tomato - 1"
+    c = Card(т=1, о=2)
+    assert repr(c) == "оот"
+    assert c.save() == "оот"
 
 def test_load():
-    t = "Tomato - 3"
+    t = "ттт"
     c = Card.load(t)
-    assert c == Card(tomato=3)
+    assert c == Card(т=3)
+
+def test_score():
+    c = Card(т=1, б=2)
+    p = Price(1, 5, 3, 2, 4)
+    assert c.score(p) == 2 + 2 * 3
